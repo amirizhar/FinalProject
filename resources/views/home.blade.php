e7d40a23c39b0bf80f6d743ecc71b1c45f45a603
amirizhar <amir_izhar@yahoo.com>
2022-12-13 00:39:21 +0800

    Use-Component
    add component for admin

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&display=swap" rel="stylesheet">
 
-    <title>Klassy Cafe - Restaurant HTML Template</title>
+    <title>FYP Management System</title>
 <!--
     
 TemplateMo 558 Klassy Cafe
@@ -42,76 +42,77 @@ https://templatemo.com/tm-558-klassy-cafe
         </div>
     </div>  
     <!-- ***** Preloader End ***** -->
-    
-    
-    <!-- ***** Header Area Start ***** -->
-    <header class="header-area header-sticky">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <nav class="main-nav">
-                        <!-- ***** Logo Start ***** -->
-                        <a href="index.html" class="logo">
-                            <img src="assets/images/klassy-logo.png" align="klassy cafe html template">
-                        </a>
-                        <!-- ***** Logo End ***** -->
-                        <!-- ***** Menu Start ***** -->
-                        <ul class="nav">
-                            <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
-                            <li class="scroll-to-section"><a href="#about">About</a></li>
-                           	
-                        <!-- 
-                            <li class="submenu">
-                                <a href="javascript:;">Drop Down</a>
-                                <ul>
-                                    <li><a href="#">Drop Down Page 1</a></li>
-                                    <li><a href="#">Drop Down Page 2</a></li>
-                                    <li><a href="#">Drop Down Page 3</a></li>
-                                </ul>
-                            </li>
-                        -->
-                            <li class="scroll-to-section"><a href="#menu">Menu</a></li>
-                            <li class="scroll-to-section"><a href="#chefs">Chefs</a></li> 
-                            <li class="submenu">
-                                <a href="javascript:;">Features</a>
-                                <ul>
-                                    <li><a href="#">Features Page 1</a></li>
-                                    <li><a href="#">Features Page 2</a></li>
-                                    <li><a href="#">Features Page 3</a></li>
-                                    <li><a href="#">Features Page 4</a></li>
-                                </ul>
-                            </li>
-                            <!-- <li class=""><a rel="sponsored" href="https://templatemo.com" target="_blank">External URL</a></li> -->
-                            <li class="scroll-to-section"><a href="#reservation">Contact Us</a></li> 
-                            <li>
-                                @if (Route::has('login'))
-                                    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                                        @auth
-                                            <li><x-app-layout>
 
-                                            </x-app-layout>
-                                            </li>
-                                        @else
-                                            <li><a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a></li>
-                    
-                                            @if (Route::has('register'))
-                                                <li><a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                                            @endif
-                                        @endauth
-                                    </div>
-                                @endif
-                            </li>
-                        </ul>        
-                        <a class='menu-trigger'>
-                            <span>Menu</span>
-                        </a>
-                        <!-- ***** Menu End ***** -->
-                    </nav>
+    <div>
+        <!-- Simplicity is an acquired taste. - Katharine Gerould -->
+            <!-- ***** Header Area Start ***** -->
+            <header class="header-area header-sticky">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <nav class="main-nav">
+                                <!-- ***** Logo Start ***** -->
+                                <a href="/" class="logo">
+                                    <img src="assets/images/5956592.png" style="height:60px"  align="klassy cafe html template">
+                                </a>
+                                <!-- ***** Logo End ***** -->
+                                <!-- ***** Menu Start ***** -->
+                                <ul class="nav">
+                                    {{-- <li class="scroll-to-section"><a href="#top" class="active">Home</a></li> --}}
+                                    {{-- <li class="scroll-to-section"><a href="#about">About</a></li> --}}
+                                       
+                                <!-- 
+                                    <li class="submenu">
+                                        <a href="javascript:;">Drop Down</a>
+                                        <ul>
+                                            <li><a href="#">Drop Down Page 1</a></li>
+                                            <li><a href="#">Drop Down Page 2</a></li>
+                                            <li><a href="#">Drop Down Page 3</a></li>
+                                        </ul>
+                                    </li>
+                                -->
+                                    {{-- <li class="scroll-to-section"><a href="#menu">Menu</a></li> --}}
+                                    {{-- <li class="scroll-to-section"><a href="#chefs">Chefs</a></li>  --}}
+                                    {{-- <li class="submenu">
+                                        <a href="javascript:;">Features</a>
+                                        <ul>
+                                            <li><a href="#">Features Page 1</a></li>
+                                            <li><a href="#">Features Page 2</a></li>
+                                            <li><a href="#">Features Page 3</a></li>
+                                            <li><a href="#">Features Page 4</a></li>
+                                        </ul>
+                                    </li> --}}
+                                    <!-- <li class=""><a rel="sponsored" href="https://templatemo.com" target="_blank">External URL</a></li> -->
+                                    <li class="scroll-to-section"><a href="#reservation">Contact Us</a></li> 
+                                    <li>
+                                        @if (Route::has('login'))
+                                            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                                                @auth
+                                                    <li><x-app-layout>
+        
+                                                    </x-app-layout>
+                                                    </li>
+                                                @else
+                                                    <li><a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a></li>
+                            
+                                                    @if (Route::has('register'))
+                                                        <li><a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                                                    @endif
+                                                @endauth
+                                            </div>
+                                        @endif
+                                    </li>
+                                </ul>        
+                                {{-- <a class='menu-trigger'>
+                                    <span>Menu</span>
+                                </a> --}}
+                                <!-- ***** Menu End ***** -->
+                            </nav>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </header>
-    <!-- ***** Header Area End ***** -->
+            </header>
+    </div>
 
     <!-- ***** Main Banner Area Start ***** -->
     <div id="top">
