939231b45ac4d7f8ebe6925815168e500d5b0da2
amirizhar <amir_izhar@yahoo.com>
2022-12-22 12:37:16 +0800

    update-duration-function

--- app/Http/Controllers/updateProject.php
+++ app/Http/Controllers/updateProject.php
@@ -12,7 +12,7 @@ class updateProject extends Controller
     function displayProject()
     {
         $output = Project::all();
-        return view('lecturer.updateProject',['senarai'=>$output]);
+        return view('lecturer.updateProject', ['senarai' => $output]);
 
         // $output = Project::select('select * from projects where supervisor = Saiful');
         // return view('lecturer.updateProject',['senarai'=>$output]);
@@ -21,30 +21,35 @@ class updateProject extends Controller
     function showProject($id)
     {
         $output = Project::find($id);
-        return view('lecturer.updatePage',['disp'=>$output]);
+        return view('lecturer.updatePage', ['disp' => $output]);
 
         // return Project::find($id);
     }
 
     function update(Request $req)
     {
-        $data=Project::find($req->id);
+        $data = Project::find($req->id);
 
         // $data->project_title=$req->project_title;
-        $data->start_date=$req->start_date;
-        $data->end_date=$req->end_date;
-        $data->duration=$req->duration;
+        $data->start_date = $req->start_date;
+
+        if ($req->end_date < $req->start_date) {
+            return 'End Date Cannot Less Then Start Date';
+        } else {
+            $data->end_date = $req->end_date;
+        }
+
+        $data->duration = $req->duration;
         // $data->student_name=$req->student_name;
         // $data->project_type=$req->project_type;
         // $data->supervisor=$req->supervisor;
         // $data->examiner_1=$req->examiner_1;
         // $data->examiner_2=$req->examiner_2;
-        $data->project_progress=$req->project_progress;
-        $data->project_status=$req->project_status;
+        $data->project_progress = $req->project_progress;
+        $data->project_status = $req->project_status;
 
         $data->save();
 
         return redirect('/showLecturer');
-
     }
 }
